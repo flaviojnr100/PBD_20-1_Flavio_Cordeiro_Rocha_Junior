512f8b60f0a16a6da9ed1eceb4c0d8e2e471238a
flaviojnr100 <45611121+flaviojnr100@users.noreply.github.com>
2021-04-11 22:47:22 -0300

    Criação das telas login e dashboard

---
+++ software_desktop/SistemaGerenciamentoPedidos/src/app/App.java
@@ -0,0 +1,34 @@
+package app;
+
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Flavio
+ */
+public class App extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent login = FXMLLoader.load(getClass().getClassLoader().getResource("view/FXMLDashBoard.fxml"));
+        Scene scene = new Scene(login);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+    
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
---
+++ software_desktop/SistemaGerenciamentoPedidos/src/controller/ControllerLogin.java
@@ -0,0 +1,35 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
+
+public class ControllerLogin {
+
+    @FXML
+    private TextField loginTxt;
+
+    @FXML
+    private PasswordField senhaTxt;
+
+    @FXML
+    private Label lblEsqueceu;
+
+    @FXML
+    private Button btnEntrar;
+
+    @FXML
+    void Entrar(ActionEvent event) {
+        
+    }
+
+    @FXML
+    void keyReleasedEntrar(KeyEvent event) {
+
+    }
+
+}
